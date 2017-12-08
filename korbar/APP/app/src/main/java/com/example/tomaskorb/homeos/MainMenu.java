2ac126998a1ddcfa0b55b187dfc96841878e8b1a
Tomas Korbar <tomas.korb@seznam.cz>
2017-12-08 14:20:36 +0100

    First commit with app

---
+++ korbar/APP/app/src/main/java/com/example/tomaskorb/homeos/MainMenu.java
@@ -0,0 +1,22 @@
+package com.example.tomaskorb.homeos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+
+
+
+
+
+
+
+
+}
