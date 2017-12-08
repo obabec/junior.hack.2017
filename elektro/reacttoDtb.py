1c35ffcc32e4ca49dc93bac86727e9a8c6962945
obabec <ond.babec@gmail.com>
2017-12-08 16:21:00 +0100

    More things

--- elektro/reacttoDtb.py
+++ elektro/reacttoDtb.py
@@ -2,7 +2,28 @@ import MySQLdb
 
 
 def getDtb():
-    db = MySQLdb.connect("localhost", "root", "95123Lo", "kappa")
+
+    #setup phase
+    svetlo_pin1 = 18
+    GPIO.setup(svetlo_pin1, GPIO.OUT)
+
+    svetlo_pin2 = 18
+    GPIO.setup(svetlo_pin2, GPIO.OUT)
+
+    svetlo_pin3 = 18
+    GPIO.setup(svetlo_pin3, GPIO.OUT)
+
+    svetlo_pin4 = 18
+    GPIO.setup(svetlo_pin4, GPIO.OUT)
+
+    svetlo_pin5 = 18
+    GPIO.setup(svetlo_pin5, GPIO.OUT)
+
+
+
+
+
+    db = MySQLdb.connect("10.10.5.234", "root", "95123Lo", "kappa")
 
     cursor = db.cursor()
     sql = "select Stav from kappaTable"
@@ -16,9 +37,8 @@ def getDtb():
         #print("SUCKERINO: "+str(k)+" "+str(row[0]))
         if currentLine == 0:
             #print(row[0])
-            svetlo_pin1 = 18
 
-            GPIO.setup(svetlo_pin1, GPIO.OUT)
+
 
             # Setting maximal F, our 10mm led has max 1500 Hz so we will use 1000 Hz
             led_pwm1 = GPIO.PWM(svetlo_pin1, 1000)
@@ -29,9 +49,7 @@ def getDtb():
 
         if currentLine == 1:
             # print("Now you are fucked for second time")
-            svetlo_pin2 = 18
 
-            GPIO.setup(svetlo_pin2, GPIO.OUT)
 
             # Setting maximal F, our 10mm led has max 1500 Hz so we will use 1000 Hz
             led_pwm2 = GPIO.PWM(svetlo_pin1, 1000)
@@ -43,9 +61,6 @@ def getDtb():
 
         if currentLine == 2:
             # print("Now 3 times")
-            svetlo_pin3 = 18
-
-            GPIO.setup(svetlo_pin3, GPIO.OUT)
 
             # Setting maximal F, our 10mm led has max 1500 Hz so we will use 1000 Hz
             led_pwm3 = GPIO.PWM(svetlo_pin3, 1000)
@@ -57,9 +72,6 @@ def getDtb():
 
         if currentLine == 3:
             #print("Now 4 times! ")
-            svetlo_pin4 = 18
-
-            GPIO.setup(svetlo_pin4, GPIO.OUT)
 
             # Setting maximal F, our 10mm led has max 1500 Hz so we will use 1000 Hz
             led_pwm4 = GPIO.PWM(svetlo_pin4, 1000)
@@ -69,9 +81,6 @@ def getDtb():
             led_pwm4.ChangeDutyCycle(int(row[0]))
 
         if currentLine == 4:
-            svetlo_pin5 = 18
-
-            GPIO.setup(svetlo_pin5, GPIO.OUT)
 
             # Setting maximal F, our 10mm led has max 1500 Hz so we will use 1000 Hz
             led_pwm5 = GPIO.PWM(svetlo_pin5, 1000)
@@ -80,9 +89,29 @@ def getDtb():
             led_pwm5.start(100)
             led_pwm5.ChangeDutyCycle(int(row[0]))
 
-        
+        if currentLine == 5:
 
-        currentLine = currentLine + 1
+
+        if currentLine == 6:
+
+
+        if currentLine == 7:
+
+
+        if currentLine == 8:
+
+
+        if currentLine == 9:
+
+
+        if currentLine == 10:
+
+
+        if currentLine == 11:
+
+
+
+      currentLine = currentLine + 1
     db.close()
 
 if __name__=='__main__':
