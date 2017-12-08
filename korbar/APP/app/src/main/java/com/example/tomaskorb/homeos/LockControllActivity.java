2aa0c40d26b1eb4712e7a5d4e6658bb9bd700398
Tomas Korbar <tomas.korb@seznam.cz>
2017-12-08 18:04:07 +0100

    Lock controll modified

---
+++ korbar/APP/app/src/main/java/com/example/tomaskorb/homeos/LockControllActivity.java
@@ -0,0 +1,15 @@
+package com.example.tomaskorb.homeos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LockControllActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_controll);
+    }
+
+
+}
