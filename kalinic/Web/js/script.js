f0e25b308ca5184e8a9e1eda48c5be9910775e8e
HonzaKali <honza.kali.os@gmail.com>
2017-12-09 00:06:30 +0100

    this is the right commit also another and yet good

---
+++ kalinic/Web/js/script.js
@@ -0,0 +1,11 @@
+function range() {
+
+	var s1 = $("#s1").val();
+	$("#l1").css("opacity", "0."+s1); 
+	var s2 = $("#s2").val();
+	$("#l2").css("opacity", "0."+s2); 
+	var s3 = $("#s3").val();
+	$("#l3").css("opacity", "0."+s3); 
+	var s4 = $("#s4").val();
+	$("#l4").css("opacity", "0."+s4); 
+}
